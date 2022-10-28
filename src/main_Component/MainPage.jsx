87ffe1812bfb35efbdf9ca99be90c31e452a5b63
Pratiyush Kumar <pratiyush.inv@gmail.com>
2022-10-28 15:45:49 +0530

    Front-end design
    Front-end design

---
+++ src/main_Component/MainPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./mainPage.css";
+
+const MainPage = () => {
+  return (
+    <div className="form__container">
+      <form action="">
+        <input
+          type="text"
+          name=""
+          id=""
+          className="inputSearch"
+          placeholder="Search...."
+        />
+        <button className="search__btn">Search Results</button>
+      </form>
+    </div>
+  );
+};
+
+export default MainPage;
